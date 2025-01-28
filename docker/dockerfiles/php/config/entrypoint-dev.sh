d439920520acd5fa2fc26ac9b10f569774423f8e
levchenko-ivan <ivan97111@yandex.ru>
2025-01-28 10:38:44 +0300

    refactor: рефактор докер файлов

--- docker/dockerfiles/php/config/entrypoint-dev.sh
+++ docker/dockerfiles/php/config/entrypoint-dev.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 set -e
 
-composer install --no-scripts
-
-php artisan migrate --force
-php artisan optimize:clear
-
 exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf &
 
 exec "$@"
